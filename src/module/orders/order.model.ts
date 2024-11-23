089389315cdc78bdd80fe29a6bb9373c8fea26bd
sakibpro2003 <sakibprodhan2003@gmail.com>
2024-11-23 16:57:37 +0600

    order model created

--- src/module/orders/order.model.ts
+++ src/module/orders/order.model.ts
@@ -1,6 +1,12 @@
-import { Schema } from "mongoose";
+import { model, Schema } from "mongoose";
 import IOrder from "./order.interface";
 
 const orderSchema = new Schema<IOrder>({
-    
-})
+  email: { type: String, required: true, unique: true },
+  product: { type: String },
+  quantity: { type: Number, required: true },
+  totalPrice: { type: Number, required: true },
+});
+
+const Order = model<IOrder>("Order", orderSchema);
+export default model;
